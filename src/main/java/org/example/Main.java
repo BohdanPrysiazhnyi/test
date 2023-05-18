359470ca310d86a045d0c980789aef07542ef6f2
bprysiazhnyi <bodacuk@gmail.com>
2023-05-18 17:22:04 +0300

    qwert

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,17 @@ public class Main {
     public static void main(String[] args) {
         int first = 1;
         int two = 2;
-        int thre = 3;
-        System.out.println(first);
+        int thre = 0;
+        char q ='q';
+        String test1 = "qwerty";
+        System.out.println(first + two + thre );
+        System.out.println(test1.toLowerCase());
+//        while (thre < 5){
+  //          System.out.println(thre);
+    //        thre++;
+      //  }
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
     }
 }
