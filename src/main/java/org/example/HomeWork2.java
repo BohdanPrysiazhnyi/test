838f52893aee21066e9f5bf23079f23fb69024b1
bprysiazhnyi <bodacuk@gmail.com>
2023-05-25 17:36:36 +0300

    Task2

--- src/main/java/org/example/HomeWork2.java
+++ src/main/java/org/example/HomeWork2.java
@@ -6,5 +6,19 @@ For a positive integer n calculate the result value, which is equal to the sum o
 Example,
 n = 1234 result = 4;
 n = 246 result = 0;*/
+    public static int task1 (int z){
+        int result = 0;
+        String lenth = Integer.toString(z);
+        int loop[] = new int[lenth.length()];
+        for (int i = 0; i <loop.length ; i++) {
+            loop [i] = lenth.charAt(i) -'0';
+        }
+        for (int i = 0; i < loop.length; i++) {
+            if (loop [i] % 2 !=0){
+                result = loop[i] + result;
+            }
 
+        }
+        return result;
+    }
 }
