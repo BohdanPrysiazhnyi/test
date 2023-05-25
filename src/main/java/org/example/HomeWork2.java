8f1093b390eade7fba23e72dca13a1055853a5b5
bprysiazhnyi <bodacuk@gmail.com>
2023-05-25 17:52:56 +0300

    Task2 second exercise

--- src/main/java/org/example/HomeWork2.java
+++ src/main/java/org/example/HomeWork2.java
@@ -21,4 +21,25 @@ n = 246 result = 0;*/
         }
         return result;
     }
+
+    /*Task 2.
+For a positive integer n calculate the result value, which is equal to the amount of the “1” in the binary
+representation of n.
+Example,
+n = 14 = 1110 result = 3;
+n = 128 = 1000 0000 result = 1;*/
+    public static int task2 (int z){
+        int result = 0;
+        String intToBinary = Integer.toBinaryString(z);
+        int[] array = new int[intToBinary.length()];
+        for (int i = 0; i < array.length; i++) {
+           array[i] = intToBinary.charAt(i)-'0';
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0){
+                result = array[i] + result;
+            }
+        }
+        return result;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,11 +4,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println(HomeWork2.task1(5234));
-
-
+        System.out.println(HomeWork2.task2(12832));
     }
-
-
 }
 
