b814c7861430bbda9ecc27ec89400257b0a325ae
bprysiazhnyi <bodacuk@gmail.com>
2023-05-25 15:10:22 +0300

    delete all

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,29 +4,9 @@ public class Main {
 
 
     public static void main(String[] args) {
-        int first = 1;
-        int two = 2;
 
-        int thre = 2;
-        char q ='q';
-        String test1 = "qwerty";
-        System.out.println(first + two + thre);
-        System.out.println(test1.toLowerCase());
 
-        while (thre < 5){
-            System.out.println(thre);
-            thre++;
-        }
 
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
-        }
-
-        if (thre < first) {
-            System.out.println("yes");
-        } else {
-            System.out.println("no");
-        }
     }
 }
 
