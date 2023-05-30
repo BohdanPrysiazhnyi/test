9e45709bb5e61c76a79b742e3c69225ee74b4299
Bogdan Prysiazhnyi <68583636+BohdanPrysiazhnyi@users.noreply.github.com>
2023-05-30 18:07:39 +0300

    Revert "Home Work 3"

--- src/main/java/org/example/HomeWork3.java
+++ src/main/java/org/example/HomeWork3.java
@@ -8,18 +8,12 @@ Example:
 {10, 5, 3, 4} =>  {4, 5, 3, 10}
 {100, 2, 3, 4, 5} => {100, 4, 3, 2, 5}
 {100, 2, 3, 45, 33, 8, 4, 54} => {54, 4, 3, 45, 33, 8, 2, 100} */
-    public static int[] task1(int[] array) {
-        int lastIndex = array.length - 1;
-        int swap;
-        for (int i = 0; i < array.length - 1; i++) {
-            int pairIndex = lastIndex - i;
-            if (pairIndex > i && array[i] % 2 == 0 && array[pairIndex] % 2 == 0){
-                swap = array[i];
-                array [i] = array[pairIndex];
-                array[pairIndex] = swap;
+    public static int [] task1 (int [] array){
+        int [] result = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0 || array[array.length-i] % 2 == 0) {
+                result [i] = array[array.length - i];
             }
         }
-        return array;
     }
-
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,10 @@
 package org.example;
 
-import java.lang.reflect.Array;
-import java.sql.SQLOutput;
-
 public class Main {
 
 
     public static void main(String[] args) {
-        int [] q ={100, 2, 3, 45, 33, 8, 4, 54};
-        HomeWork3.task1(q);
-        for (int i = 0; i < HomeWork3.task1(q).length; i++) {
-            System.out.println(HomeWork3.task1(q)[i]);
-        }
+        System.out.println(HomeWork2.task3(11));
     }
 }
 
