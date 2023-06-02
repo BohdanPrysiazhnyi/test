2f0cb649843644d4e34563efe15521c5d77a64fa
bprysiazhnyi <bodacuk@gmail.com>
2023-06-02 16:15:09 +0300

    Merge remote-tracking branch 'Test/master'

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 import home.work.four.SortOrder;
 import home.work.four.Task1;
 import home.work.four.Task2;
+import home.work.four.Task4;
 
 import java.lang.reflect.Array;
 import java.sql.SQLOutput;
@@ -11,13 +12,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-        int[] array = {15, 10, 3};
-        SortOrder order = SortOrder.DESC;
-        System.out.println(Task1.sortOrder(array, order));
-
-        for (int q: Task2.transform(array,order)) {
-            System.out.println(q);
-        }
+        System.out.println(Task4.progression(5,3,4));
 
     }
 
